0aec01d738712050706a2c5859e3b37a3a0274c4
Eron Castro <eroncastro@gmail.com>
2019-05-18 12:54:17 -0300

    Enhanced helpers

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -2,8 +2,8 @@ export function formatDate(timestamp) {
   const d = new Date(timestamp);
   const time = d.toLocaleTimeString('en-US');
   return time.substr(0, 5) + time.slice(-2) + ' | ' + d.toLocaleDateString();
-};
+}
 
 export function generateId() {
   return Math.random().toString(36).substring(2) + (new Date()).getTime().toString(36);
-};
+}
